379c7d0a358ad110e0b1dd3d2858d8a2b330fcef
NetoMarcal <neto.marcal.dev@gmail.com>
2019-12-18 10:39:58 -0300

    Implementação do group de routes inicial

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+/** @var \Laravel\Lumen\Routing\Router $router */
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->group(['prefix' => '/api'], function () use ($router){
+
 });
